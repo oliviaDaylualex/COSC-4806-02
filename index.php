bcc260373771f94dab825291a5123a43dd2906b4
oliviaDaylualex <oliviaalex799@gmail.com>
2025-07-16 23:16:50 +0000

    updated index page with login and register  buttons

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+
+<h1>Welcome to Your Login System</h1>
+
+    <?php
+    if (isset($_SESSION['username'])) {
+        echo '<p>Hello, ' . $_SESSION['username'] . '! <a href="logout.php">Logout</a></p>';
+    } else {
+        echo '<p><a href="login.php">Login</a> | <a href="register.php">Register</a></p>';
+    }
+    ?>
+
+
+</body>
 </html>
