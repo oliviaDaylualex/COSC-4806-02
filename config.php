035cba2bcd8b4e2a641c7752725f39028b8343f1
oliviaDaylualex <oliviaalex799@gmail.com>
2025-07-16 23:08:41 +0000

    added database and names to config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'hpg0z.h.filess.io');
+define('DB_USER', 'COSC4806_poledawnby');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', 'COSC4806_poledawnby');
+define('DB_PORT', '3305');
+
+?>
